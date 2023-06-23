01f3477d10bf17c5109c5a0a379aa0765607844d
niks <niks>
2023-06-23 14:53:36 +0300

    Izveidots Main menu

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnSettingsButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
